50186c2c4b9f82b6cf9ffd035f1a766be4fa1674
franciscui <franciscui@lagou.com>
2016-04-09 19:33:49 +0800

    df

--- base/baselog.py
+++ base/baselog.py
@@ -3,12 +3,14 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
 from function import switch
-#from logging.handlers import RotatingFileHandler
-#log_file_handler = TimedRotatingFileHandler(filename="ds_update", when="M", interval=2, backupCount=2)
+import sys
+import os
+import stat
+
 
 class BaseLog(object):
 
-	_level = {
+    _level = {
 		'CRITICAL': logging.CRITICAL,
 		'DEBUG': logging.DEBUG,
 		'ERROR': logging.ERROR,
@@ -19,44 +21,67 @@ class BaseLog(object):
 		'NOTSET': logging.NOTSET
 	}
 
-	def __init__(self,log_level,log_path,log_when,log_backupCount,log_rotating_interval,name=__name__):
-                log_format = '%(asctime)s %(levelname)s %(threadName)s %(filename)s[line:%(lineno)d] %(message)s'
-		self._logger = logging.getLogger(name)
-		self._logger.setLevel(self._level[log_level.upper()])
-		for h in self._logger.handlers:
-			self._logger.removeHandler(h)
-		handler = TimedRotatingFileHandler(filename=log_path, when=log_when, interval=eval(log_rotating_interval), backupCount=eval(log_backupCount))
-		#handler = logging.FileHandler(log_path)
-		handler.setLevel(self._level[log_level.upper()])
-		formatter = logging.Formatter(log_format)
-		handler.setFormatter(formatter)
-		self._logger.addHandler(handler)
-	
-	def writelog(self,log_level,message):
-		for case in switch(log_level.strip().lower()):
-			if case('warn'):
-				self.warn(message)
-				break
-			if case('info'):
-				self.info(message)
-				break
-			if case('error'):
-				self.error(message)
-				break
-			if case('debug'):
-				self.debug(message)
-				break
-			if case():
-				break
+    def __init__(self,log_level,log_path,log_file,log_when,log_backupCount,log_rotating_interval,name=__name__):
+        log_format = '%(asctime)s %(levelname)s %(threadName)s %(filename)s[line:%(lineno)d] %(message)s'
+        self._logger = logging.getLogger(name)
+        self._logger.setLevel(self._level[log_level.upper()])
+        self._logdir = log_path
+        self._logbase = self._logdir + log_file
+        self._create_file()
+        self._mod_authority()
+        for h in self._logger.handlers:
+            self._logger.removeHandler(h)
+        handler = TimedRotatingFileHandler(filename=self._logbase, when=log_when, interval=eval(log_rotating_interval), backupCount=eval(log_backupCount))
+        handler.setLevel(self._level[log_level.upper()])
+        formatter = logging.Formatter(log_format)
+        handler.setFormatter(formatter)
+        self._logger.addHandler(handler)
 
-	def info(self, message):
-		self._logger.info(message)
+    def _create_file(self):
+        '''Create file for logging.'''
+        try:
+            if os.path.isdir(self._logdir) is False:
+                os.makedirs(self._logdir)
+            if os.path.exists(self._logbase) is False:
+                open(self._logbase, 'w').close()
+        except (OSError, IOError, Exception),msg:
+            sys.stderr.write('_LogConfig:%s'%msg)
+            sys.exit(-1)
 
-	def warn(self, message):
-		self._logger.warn(message)
+    def _mod_authority(self):
+        '''Modify file authority'''
+        try:
+            os.chmod(self._logdir, stat.S_IRWXU|stat.S_IRWXG|stat.S_IRWXO)
+            os.chmod(self._logbase, stat.S_IRWXU|stat.S_IRWXG|stat.S_IRWXO)
+        except Exception,msg:
+            sys.stderr.write('User does not have '\
+            'modify permissions, msg:%s\n'%msg)
 
-	def error(self, message):
-		self._logger.error(message)
+    def writelog(self,log_level,message):
+        for case in switch(log_level.strip().lower()):
+            if case('warn'):
+                self.warn(message)
+                break
+            if case('info'):
+                self.info(message)
+                break
+            if case('error'):
+                self.error(message)
+                break
+            if case('debug'):
+                self.debug(message)
+                break
+            if case():
+                break
 
-	def debug(self, message):
-		self._logger.debug(message)
+    def info(self, message):
+        self._logger.info(message)
+
+    def warn(self, message):
+        self._logger.warn(message)
+
+    def error(self, message):
+        self._logger.error(message)
+
+    def debug(self, message):
+        self._logger.debug(message)
